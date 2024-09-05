aaf501a9b53fd2bedc99dfb7d36c4df3869f2dec
Kauã Fellipe <82785675+kfellipe@users.noreply.github.com>
2024-09-05 17:04:13 -0300

    Update update-dns-on-start.sh

--- Shell/update-dns-on-start.sh
+++ Shell/update-dns-on-start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 read -p "Seu dominio(SEM ESPAÇOS): " DOMAIN
-
+read -p "Usuario: " NOIPUSER
+read -p "Chave: " NOIPPASS
 # Criando script para atualizar dominio DNS
 
 cat <<EOF>/home/ec2-user/update-domain.sh
 #!/bin/bash
 
 IPEC2=\`curl http://checkip.amazonaws.com\`
-curl http://z5yxwcg:aVoSWUFkn2ss\\@dynupdate.no-ip.com/nic/update\\?hostname\\=semanaaws.zapto.org\\&myip\\=$IPEC2
+curl http://$NOIPUSER:$NOIPPASS\\@dynupdate.no-ip.com/nic/update\\?hostname\\=semanaaws.zapto.org\\&myip\\=\$IPEC2
 
 EOF
 
