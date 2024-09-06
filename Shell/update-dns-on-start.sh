db3d4f4e3cd88ab0503efd0fafb59f1f2fa24944
Kauã Fellipe <82785675+kfellipe@users.noreply.github.com>
2024-09-06 10:45:12 -0300

    Update update-dns-on-start.sh

--- Shell/update-dns-on-start.sh
+++ Shell/update-dns-on-start.sh
@@ -14,7 +14,7 @@ cat <<EOF>/home/ec2-user/update-domain.sh
 #!/bin/bash
 
 IPEC2=\`curl http://checkip.amazonaws.com\`
-curl http://$NOIPUSER:$NOIPPASS\\@dynupdate.no-ip.com/nic/update\\?hostname\\=semanaaws.zapto.org\\&myip\\=\$IPEC2
+curl http://$NOIPUSER:$NOIPPASS\\@dynupdate.no-ip.com/nic/update\\?hostname\\=$DOMAIN\\&myip\\=\$IPEC2
 
 EOF
 
